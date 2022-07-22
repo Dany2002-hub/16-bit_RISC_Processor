350ab89e922e309acee004aaf6bc39a3dbfe6bd1
Dany2002-hub <76448256+Dany2002-hub@users.noreply.github.com>
2022-07-22 12:57:30 +0530

    Create parameters.v

---
+++ parameters.v
@@ -0,0 +1,10 @@
+`ifndef PARAMETER_H_
+`define PARAMETER_H_
+
+`define col 16 // 16 bits Instruction and data memory 
+`define row_inst 14 // I have taken 14 instructions as the parameter
+`define row_data 8 // Number of data in the data memory
+
+`define filename "dump.vcd"
+`define simulation_time #200
+`endif
